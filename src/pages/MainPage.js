4f355dee27e0f2278b4331868df93190d4691b8a
unknown <lerle144@gmail.com>
2022-08-28 15:08:06 +0900

    프로젝트 기본 세팅, App.js 라우터 기본 세팅

---
+++ src/pages/MainPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const MainPage = ({ history }) => {
+
+
+    return (
+        <>
+        
+        <p>메인페이지입니다.</p>
+        
+        </>
+    );
+};
+
+export default MainPage;
