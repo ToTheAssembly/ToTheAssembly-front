9f0d05a12e68256a37319853f95e54b6fa790a7b
unknown <lerle144@gmail.com>
2022-10-02 02:37:49 +0900

    TrendPage 의원 네트워크 svg

--- src/pages/TrendPage.js
+++ src/pages/TrendPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import MemberNetwork from '../components/MemberNetwork';
 
 
 const PageContainer = styled.div`
@@ -7,6 +8,13 @@ const PageContainer = styled.div`
   max-width: 1000px;
 `;
 
+const Title = styled.div`
+    margin: 20px;
+    color: #49446B;
+    font-size: 30px;    
+    font-family: Pretendard;
+    font-weight: bold;
+`
 
 // 트렌드 분석
 const TrendFrame = styled.div`
@@ -17,19 +25,22 @@ const TrendFrame = styled.div`
 
 // 의원 네트워크
 const NetworkFrame = styled.div`
-    background: gray;
+    background: #EDF4FA;
     width: 1000px;
-    height: 600px;
+    min-height: 600px;
 `
 
 const TrendPage = () => {
   return (
     <PageContainer>
+      <Title>토픽 트렌드</Title>
       <TrendFrame>
         
       </TrendFrame>
+      <Title>의원 네트워크</Title>
       <NetworkFrame>
-      <iframe title="의원 네트워크" width="1000" height="600" frameBorder="0" scrolling="no" src="http://localhost:8000/network/">의원 네트워크</iframe>
+        <iframe title="의원 네트워크" width="1000" height="600" frameBorder="0" scrolling="no" src="http://localhost:8000/network/">의원 네트워크</iframe>
+        <MemberNetwork width={1000} height={1000} fill="#ffffff" />
       </NetworkFrame>
     </PageContainer>
   )
